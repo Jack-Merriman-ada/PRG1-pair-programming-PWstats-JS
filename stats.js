e93e550ea9bd9efed97771d9962e75f0d78c4a3e
Jack-Merriman-ada <jack.merriman@ada.ac.uk>
2024-10-01 10:51:26 +0000

    refactor

--- stats.js
+++ stats.js
@@ -10,49 +10,24 @@ function deleteExistingOutputFile() {
   }
 }
 
-// function processData() {
-//   const data = fs.readFileSync(inputFile, "utf-8");
-//   const lines = data.split(/\n/);
-
-//   for (line of lines) {
-//     elements = line.split(delimiter);
-//     console.log(elements);
-//   }
-// }
-
 
 function processData() {
   const data = fs.readFileSync(inputFile, "utf-8");
   // console.log(data)
   const lines = data.split(/\n/);
-  let passwordLengthArray = [];
 
-  const newObject = {
-  }
+  const newObject = {}
 
   for (const line of lines) { // loop through each line in lines
-    let elements = line.split(delimiter); //split line into an array
+    let elements = line.split(delimiter); //split line into an array called elements
     const passwordLength = elements[1].length // access the password in line and return its length
-    passwordLengthArray.push(passwordLength) // push the length to password length array
 
-    if (newObject[`${passwordLength} chars long`]) {
-      newObject[`${passwordLength} chars long`] += 1
+    if (newObject[`${passwordLength} chars long`]) { // checks new object for key
+      newObject[`${passwordLength} chars long`] += 1 // if key exists, increases value by one
     } else {
-      newObject[`${passwordLength} chars long`] = 1
+      newObject[`${passwordLength} chars long`] = 1 // else creates key and value 
     }
   }
-
-  
-
-  // for (const number in passwordLengthArray) {
-  //   console.log(newObjectPropertiesArray)
-  //   if (newObjectPropertiesArray.includes(`${number} chars long`)) {
-  //     newObject[`${number} chars long`] += 1;
-  //   } else {
-  //     newObject[`${number} chars long`] = 0;
-  //     newObject[`${number} chars long`] += 1;
-  //   }
-  // }
 
   console.log(newObject)
 }
